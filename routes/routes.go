4eece5cb8e010e9d61182b866f5ef415b51affe2
ef3d0c3e <ef3d0c3e@pundalik.org>
2026-09-08 23:22:13 +0200

    refactor: remove dead code

--- routes/routes.go
+++ routes/routes.go
@@ -1,41 +1,12 @@
 package routes
 
 import (
-	"bytes"
-	"html/template"
-	"net/http"
 	"transcendance/controllers"
 	"transcendance/views"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
-
-var templates = template.Must(
-	template.ParseGlob("templates/**/*.html"),
-)
-
-// Render template `page` using the base template.
-func renderPage(c *gin.Context, page string, data any) {
-	var content bytes.Buffer
-
-	err := templates.ExecuteTemplate(&content, page, data)
-	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	viewData := map[string]any{
-		"Title":   data.(map[string]any)["Title"],
-		"Content": template.HTML(content.String()),
-	}
-
-	err = templates.ExecuteTemplate(c.Writer, "base", viewData)
-	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-}
 
 
 func RegisterRoutes(
